3dbfec0476b6aef828ce9abdcedf696cf016b8cf
Stan125 <stanislaus@stadlmann.cm>
2017-07-17 15:52:32 +0200

    return nothing if no bamlss object found

--- R/search_bamlss.R
+++ R/search_bamlss.R
@@ -9,7 +9,11 @@ search_ba <- function() {
   # Look for bamlss components
   bamlss_true <- sapply(lsc, FUN = function(x) {
     return(any(class(get(x)) == "bamlss"))
-    })
+  })
 
-  return(lsc[bamlss_true])
+  if (length(bamlss_true) > 0)
+    if (sum(bamlss_true) > 0)
+      return(lsc[bamlss_true])
+  else
+    return("")
 }
