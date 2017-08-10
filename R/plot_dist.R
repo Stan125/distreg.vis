d3d8bdd1b663e9687f172433b1b2ac7f97abce49
Stan125 <stanislaus@stadlmann.cm>
2017-08-10 15:03:31 +0200

    Create own prediction function

--- R/plot_dist.R
+++ R/plot_dist.R
@@ -11,35 +11,8 @@ limits <- function(x, times_sd = 3)
 plot_dist <- function(model, newdata, palette = "default",
                       type = "pdf") {
 
-  # Stop if no intercept
-  if (is.null(newdata$intercept))
-    stop("Intercept not specified")
-
-  # Omit NA's
-  newdata <- na.omit(newdata)
-
-  # Two df's with and without intercept
-  p_m_i <- data.frame()
-  p_m_ni <- data.frame()
-
-  # Get predictions for obs with intercept
-  if (sum(newdata$intercept) != 0) {
-    tempdata <- subset(newdata, intercept == TRUE)
-    p_m_i <- as.data.frame(predict(model, tempdata, type = "parameter",
-                                   intercept = TRUE))
-    row.names(p_m_i) <- row.names(tempdata)
-  }
-  # Get preds for obs without intercept
-  if (sum(newdata$intercept) != nrow(newdata)) {
-    tempdata <- subset(newdata, intercept == FALSE)
-    p_m_ni <- as.data.frame(predict(model, tempdata, type = "parameter",
-                                    intercept = FALSE))
-    row.names(p_m_ni) <- row.names(tempdata)
-  }
-
-  # Put both together
-  p_m <- rbind(p_m_i, p_m_ni)
-  p_m <- p_m[order(row.names(p_m)), ]
+  # Get predictions for newdata
+  p_m <- preds(model, newdata)
 
   # Get family and function for pdf
   fam_gen <- family(model)
---
+++ R/preds.R
@@ -0,0 +1,37 @@
+#' Handle predictions in bamlss case (including intercept as variable)
+#'
+
+preds <- function(model, newdata) {
+  # Stop if no intercept
+  if (is.null(newdata$intercept))
+    stop("Intercept not specified")
+
+  # Omit NA's
+  newdata <- na.omit(newdata)
+
+  # Two df's with and without intercept
+  p_m_i <- data.frame()
+  p_m_ni <- data.frame()
+
+  # Get predictions for obs with intercept
+  if (sum(newdata$intercept) != 0) {
+    tempdata <- subset(newdata, intercept == TRUE)
+    p_m_i <- as.data.frame(predict(model, tempdata, type = "parameter",
+                                   intercept = TRUE))
+    row.names(p_m_i) <- row.names(tempdata)
+  }
+  # Get preds for obs without intercept
+  if (sum(newdata$intercept) != nrow(newdata)) {
+    tempdata <- subset(newdata, intercept == FALSE)
+    p_m_ni <- as.data.frame(predict(model, tempdata, type = "parameter",
+                                    intercept = FALSE))
+    row.names(p_m_ni) <- row.names(tempdata)
+  }
+
+  # Put both together
+  p_m <- rbind(p_m_i, p_m_ni)
+  p_m <- p_m[order(row.names(p_m)), ]
+
+  # Return it here
+  return(p_m)
+}
