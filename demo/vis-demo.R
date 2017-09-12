39bd305b72c8700ab4b70d1a92a8d2c022c584cb
Stan125 <stanislaus@stadlmann.cm>
2017-09-12 12:18:34 +0200

    Change demo so it represents mvnorm too

--- demo/vis-demo.R
+++ demo/vis-demo.R
@@ -3,6 +3,7 @@ library(bamlss.vis)
 
 # Data
 data_fam <- model_fam_data()
+art_data <- GAMart()
 
 # Models
 beta_model <- bamlss(list(beta ~ norm1 + norm2,
@@ -11,6 +12,10 @@ beta_model <- bamlss(list(beta ~ norm1 + norm2,
 gamma_model <- bamlss(list(gamma ~ norm1 + norm2,
                            sigma ~ norm1 + norm2),
                       data = data_fam, family = gamma_bamlss())
+mvnorm_model <- bamlss(list(cbind(num, err) ~ x1 + x2 + x3,
+                            sigma1 ~ 1,
+                            sigma2 ~ 1), data = art_data,
+                       family = mvnorm_bamlss(k = 2))
 
 # Start the App
 bamlss.vis::vis()
