4a8d9016a51cff097f20b68d1645b31d2a519e38
Stan125 <stanislaus@stadlmann.cm>
2018-08-22 15:09:51 +0200

    Change poisson limit and make limits in Excel numeric

--- data-raw/table_to_rda.R
+++ data-raw/table_to_rda.R
@@ -1,3 +1,8 @@
-dists <- readxl::read_excel("inst/extdata/dist_df.xlsx")
-dists <- as.data.frame(dists)
+library("dplyr")
+library("readxl")
+suppressWarnings({
+  dists <- read_excel("inst/extdata/dist_df.xlsx") %>%
+    as.data.frame() %>%
+    mutate_at(vars(l_limit:u_limit), .funs = as.numeric)
+})
 save(list = "dists", file = "data/dists.rda")
