79d76d54ffd7ada3ff93d6dd8a942e0455bf270d
Stan125 <stanislaus@stadlmann.cm>
2018-08-28 16:25:26 +0200

    xlsx -> csv for dealing with new distributions

--- data-raw/table_to_rda.R
+++ data-raw/table_to_rda.R
@@ -1,10 +1,9 @@
-library("dplyr")
-library("readxl")
-suppressWarnings({
-  dists <- read_excel("inst/extdata/dist_df.xlsx") %>%
-    as.data.frame() %>%
-    mutate_at(vars(l_limit:u_limit), .funs = as.numeric)
-})
-write.table(dists, file = "inst/extdata/dist_df.csv",
-            sep = ";", dec = ".", row.names = FALSE)
+dists <-
+  read.table(
+    "inst/extdata/dist_df.csv",
+    sep = ";",
+    dec = ".",
+    header = TRUE,
+    stringsAsFactors = FALSE
+  )
 save(list = "dists", file = "data/dists.rda")
