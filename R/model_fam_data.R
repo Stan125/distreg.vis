102abac48556b9b3fc1b5177dee3cc75417147a7
Stan125 <stanislaus@stadlmann.cm>
2018-08-31 12:10:08 +0200

    glogis now vectorized

--- R/model_fam_data.R
+++ R/model_fam_data.R
@@ -78,8 +78,7 @@ model_fam_data <- function(nrow = 500, seed = 1408, fam_name = "NO") {
       tfvec <- qpois(u_data$v1, 3)
 
     if (fam_name == "glogis")
-      tfvec <- sapply(u_data$v1, FUN = glogis_bamlss()$q,
-                      par = list(mu = 1, sigma = 1, alpha = 1)) # not vectorized
+      tfvec <- qlogis_bamlss()$q(u_data$v1, list(mu = 1, sigma = 1, alpha = 1))
 
   }
 
