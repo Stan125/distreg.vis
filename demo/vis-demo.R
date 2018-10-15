c76c73ff8bf91a610a9f5a52d3d4b6f4142b3edb
Stan125 <stanislaus@stadlmann.cm>
2018-10-15 11:14:09 +0200

    Change demos

--- demo/vis-demo.R
+++ demo/vis-demo.R
@@ -1,21 +1,24 @@
  ## Demo: Shiny App
-library(distreg.vis)
+library("distreg.vis")
+library("gamlss")
+library("bamlss")
 
-# Data
-data_fam <- model_fam_data()
+# A few models
+normal_gamlss <- gamlss(NO ~ binomial1 + ps(norm2),
+                 sigma.formula = ~ binomial1 + ps(norm2),
+                 data = model_fam_data(),
+                 trace = FALSE)
 
-# Models
-beta_model <- bamlss(list(beta ~ norm1 + norm2,
-                          sigma2 ~ norm1 + norm2),
-                     data = data_fam, family = beta_bamlss())
-poisson_model <- bamlss(list(poisson ~ norm1 + norm2),
-                        data = data_fam, family = poisson_bamlss())
-mvnorm_model <- bamlss(list(normal ~ s(norm2),
-                            norm1 ~ s(norm2)), data = data_fam,
-                       family = mvnorm_bamlss(k = 2))
-# multinomial_model <- bamlss(list(multinomial ~ norm1 + norm2 + binomial1),
-#                             data = data_fam,
-#                             family = multinomial_bamlss())
+beta_bamlss <- bamlss(list(beta ~ binomial1 + s(norm2),
+                    ~ binomial1 + s(norm2)),
+               data = model_fam_data(fam_name = "beta"),
+               family = beta_bamlss(),
+               verbose = FALSE)
+
+multinomial_bamlss <- bamlss(list(multinomial ~ binomial1 + s(norm2)),
+                      data = model_fam_data(fam_name = "multinomial"),
+                      family = multinomial_bamlss(),
+                      verbose = FALSE)
 
 # Start the App
 distreg.vis::vis()
